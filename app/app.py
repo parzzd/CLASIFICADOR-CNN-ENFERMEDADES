dcf165f01a8acae6ec9bbf87dc3433912ca975a7
Pedro RD <pedro.ard1212@gmail.com>
2023-11-10 10:23:23 -0500

    main

--- app/app.py
+++ app/app.py
@@ -45,22 +45,35 @@ def index():
 def predict_image(image_path):
     # Carga la imagen y realiza preprocesamiento si es necesario
     img = cv2.imread(image_path)
-    img = cv2.resize(img, (300, 300))  # Ajusta el tamaño según lo entrenado
-
-    # Normaliza la imagen si es necesario
-    img = img / 255.0  # Normaliza los valores de píxeles al rango [0, 1]
+    if img is not None:
+        # Convertir imágenes en escala de grises a imágenes en color
+        if len(img.shape) == 2:
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        img = cv2.resize(img, (264, 264))
 
     # Realiza la predicción utilizando el modelo
     result = model.predict(np.array([img]))[0]
 
-    # Decodifica el resultado y obtén la etiqueta
-    labels = ["Pneumothorax", "NoFinding", "Infiltration", "Nodule", "Pleural_Thickening", "Effusion"]
-    predicted_label = labels[np.argmax(result)]
+    # # Decodifica el resultado y obtén la etiqueta
+    # labels = ["Pneumothorax", "NoFinding", "Infiltration", "Nodule", "Pleural_Thickening", "Effusion"]
+    # predicted_label = labels[np.argmax(result)]
 
+    if result.argmax()==0:
+        predicted_label=("pneumo")
+    elif result.argmax()==1:
+        predicted_label=("pleural")
+    elif result.argmax()==2:
+        predicted_label=("nofinding")  
+    elif result.argmax()==3: 
+        predicted_label=("NODULE")
+    elif result.argmax()==4:
+        predicted_label=("INFILTRATION")
+    elif result.argmax()==5:
+        predicted_label=("EFFUSION")        
     return f'La IA predice que la imagen pertenece a la categoría: {predicted_label}', image_path
 
 
 if __name__ == '__main__':
     # Carga el modelo de IA
-    model = tf.keras.models.load_model('app/modelo.keras')
+    model = tf.keras.models.load_model('app/modelo.h5')
     app.run(debug=True, port=5002)
