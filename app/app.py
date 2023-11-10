a475fafaa43c2b9d8195b9ad14287875a8eff509
Pedro RD <pedro.ard1212@gmail.com>
2023-11-10 10:11:44 -0500

    main

--- app/app.py
+++ app/app.py
@@ -3,40 +3,52 @@ import cv2
 import numpy as np
 import tensorflow as tf
 import os
+
 app = Flask(__name__)
 model = None  # Variable para almacenar el modelo de IA
 
-# Ruta para cargar la página principal
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    result = None
+    image_path = None
+
     if request.method == 'POST':
         # Verifica si se ha enviado un archivo
         if 'file' not in request.files:
             return redirect(request.url)
-        
+
         file = request.files['file']
-        
+
         # Verifica si el archivo tiene un nombre y es una imagen
         if file.filename == '':
             return redirect(request.url)
-        
+
         if file:
             # Guarda la imagen en la carpeta 'uploads' en la ruta estática
-            file_path = 'static/uploads/' + file.filename
+            upload_folder = 'app/static/uploads/'
+
+            os.makedirs(upload_folder, exist_ok=True)
+
+            file_path = os.path.join(upload_folder, file.filename)
             file.save(file_path)
 
             # Realiza la predicción utilizando el modelo de IA
-            result = predict_image(file_path)
+            result, image_filename = predict_image(file_path)
 
-            return render_template('index.html', result=result)
+            # Construye la ruta completa a la imagen para la plantilla HTML
+            image_path = os.path.join('uploads', image_filename)
 
-    return render_template('index.html', result=None)
+    return render_template('index.html', result=result, image_path=image_path)
+
 
 # Función para realizar la predicción en una imagen
 def predict_image(image_path):
     # Carga la imagen y realiza preprocesamiento si es necesario
     img = cv2.imread(image_path)
-    img = cv2.resize(img,(264, 264))  # Ajusta el tamaño según lo entrenado
+    img = cv2.resize(img, (300, 300))  # Ajusta el tamaño según lo entrenado
+
+    # Normaliza la imagen si es necesario
+    img = img / 255.0  # Normaliza los valores de píxeles al rango [0, 1]
 
     # Realiza la predicción utilizando el modelo
     result = model.predict(np.array([img]))[0]
@@ -45,13 +57,10 @@ def predict_image(image_path):
     labels = ["Pneumothorax", "NoFinding", "Infiltration", "Nodule", "Pleural_Thickening", "Effusion"]
     predicted_label = labels[np.argmax(result)]
 
-    return f'La IA predice que la imagen pertenece a la categoría: {predicted_label}'
+    return f'La IA predice que la imagen pertenece a la categoría: {predicted_label}', image_path
+
 
 if __name__ == '__main__':
-    
-    # custom_objects = {'Adam': tf.keras.optimizers.Adam}
-    #custom_objects=custom_objects
-    model = tf.keras.models.load_model('/Users/pedrord/Documents/GitHub/IA-CLASIFICADOR/app/modelo.h5')
+    # Carga el modelo de IA
+    model = tf.keras.models.load_model('app/modelo.keras')
     app.run(debug=True, port=5002)
-
-    #250 8magense
--- app/ia.py
+++ app/ia.py
@@ -1,99 +1,95 @@
 import tensorflow as tf
-from keras import layers,models
+from keras import layers, models
 import os
 import numpy as np
 import cv2
-#dimension 264x264
-#pip install --upgrade tensorflow
-#pip install --upgrade keras
-
-#model.save('model.h5')
-#loaded_model = tf.keras.models.load_model('model.h5')
-
+from sklearn.model_selection import train_test_split
 
 width = 264
 height = 264
-rut_entrada = "app/imagenes/entrada/"
-rut_prueba = "app/imagenes/test"
+
+rut_entrada = "app\IMAGENES\entrada"
+rut_prueba = "app\IMAGENES\test"
 
 entrenamiento_x = []
 entrenamiento_y = []
 
-for i in os.listdir(rut_entrada):
-    for j in os.listdir(rut_entrada + i):
-        img = cv2.imread(rut_entrada + i + "/" + j)
-        resized_image = cv2.resize(img, (width, height))
+# for clase in os.listdir(rut_entrada):
+#     clase_path = os.path.join(rut_entrada, clase)
+#     if os.path.isdir(clase_path):  # Asegurarse de que es un directorio
+#         for imagen_nombre in os.listdir(clase_path):
+#             img_path = os.path.join(clase_path, imagen_nombre)
+#             img = cv2.imread(img_path)
+#             if img is not None:
+#                 # Convertir imágenes en escala de grises a imágenes en color
+#                 if len(img.shape) == 2:
+#                     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+#                 resized_image = cv2.resize(img, (width, height))
+#                 entrenamiento_x.append(resized_image)
+#                 # Construir las etiquetas para cada imagen
+#                 etiqueta = [0] * 6
+#                 if clase == "Effusion":
+#                     etiqueta[5] = 1
+#                 elif clase == "Infiltration":
+#                     etiqueta[4] = 1
+#                 elif clase == "Nodule":
+#                     etiqueta[3] = 1
+#                 elif clase == "NoFinding":
+#                     etiqueta[2] = 1
+#                 elif clase == "Pleural_Thickening":
+#                     etiqueta[1] = 1
+#                 elif clase == "Pneumothorax":
+#                     etiqueta[0] = 1
+#                 entrenamiento_y.append(etiqueta)
+#             else:
+#                 print(f"Error al cargar la imagen: {img_path}")
 
-        entrenamiento_x.append(resized_image)
-        if i == "Effusion":
-            entrenamiento_y.append([0, 0, 0, 0, 0, 1])
-        elif i == "Infiltration":
-            entrenamiento_y.append([0, 0, 0, 0, 1, 0])
-        elif i == "Nodule":
-            entrenamiento_y.append([0, 0, 0, 1, 0, 0])
-        elif i == "NoFinding":
-            entrenamiento_y.append([0, 0, 1, 0, 0, 0])
-        elif i == "Pleural_Thickening":
-            entrenamiento_y.append([0, 1, 0, 0, 0, 0])
-        elif i == "Pneumothorax":
-            entrenamiento_y.append([1, 0, 0, 0, 0, 0])
+# x_data = np.array(entrenamiento_x)
+# y_data = np.array(entrenamiento_y)
 
-x_data = np.array(entrenamiento_x)
-y_data = np.array(entrenamiento_y)
+# # Dividir los datos en conjuntos de entrenamiento y prueba
+# x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=42)
 
-model = tf.keras.Sequential([
-    layers.Conv2D(64, 3, 3, input_shape=(width, height, 3), activation='relu'),
-    layers.MaxPooling2D(pool_size=(2, 2)),
+# model = tf.keras.Sequential([
+#     layers.Conv2D(64, 3, 3, input_shape=(width, height, 3), activation='relu'),
+#     layers.MaxPooling2D(pool_size=(2, 2)),
 
-    layers.Conv2D(128, 3, 3, activation='relu'),
-    layers.MaxPooling2D(pool_size=(2, 2)),
+#     layers.Conv2D(128, 3, 3, activation='relu'),
+#     layers.MaxPooling2D(pool_size=(2, 2)),
 
-    layers.Conv2D(256, 3, 3, activation='relu'),
-    layers.MaxPooling2D(pool_size=(2, 2)),
+#     layers.Conv2D(256, 3, 3, activation='relu'),
+#     layers.MaxPooling2D(pool_size=(2, 2)),
 
-    layers.Dropout(0.5),
-    layers.Flatten(),
+#     layers.Dropout(0.5),
+#     layers.Flatten(),
+#     layers.Dense(6, activation='sigmoid')
+# ])
 
-    layers.Dense(128, activation='relu'),
-    layers.Dense(6, activation='sigmoid')
-])
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# epoch = 100
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model.fit(x_train, y_train, epochs=epoch, validation_data=(x_test, y_test))
 
+# # Guardar el modelo en una ruta relativa o absoluta
+# model.save('modelo.h5')
 
-epoch = 100
+model = tf.keras.models.load_model('modelo.h5')
 
-model.fit(x_data, y_data, epochs=epoch)
+imagen=cv2.imread("D:/CLASIFICADOR-CNN-ENFERMEDADES/app/IMAGENES/test/00001373_038.png")
+imagen=cv2.resize(imagen,(width,height))
 
-model.save('modelo.h5') # Corregida la función de guardar el modelo
+result=model.predict(np.array([imagen]))[0]
 
-
-
-
-
-
-
-
-
-# model = tf.keras.models.load_model('/Users/pedrord/Documents/GitHub/IA-CLASIFICADOR/app/modelo.h5')
-
-# imagen=cv2.imread("/Users/pedrord/Documents/GitHub/IA-CLASIFICADOR/app/imagenes/test/00001376_008.png")
-# imagen=cv2.resize(imagen,(width,height))
-
-# result=model.predict(np.array([imagen]))[0]
-
-# if result.argmax()==0:
-#     print("pneumo")
-# elif result.argmax()==1:
-#     print("pleural")
-# elif result.argmax()==2:
-#     print("nofinding")  
-# elif result.argmax()==3:
-#     print("NODUEL")
-# elif result.argmax()==4:
-#     print("INFILTRATION")
-# elif result.argmax()==5:
-#     print("EFFUSION")    dsd
-    
+if result.argmax()==0:
+    print("pneumo")
+elif result.argmax()==1:
+    print("pleural")
+elif result.argmax()==2:
+    print("nofinding")  
+elif result.argmax()==3: 
+    print("NODUEL")
+elif result.argmax()==4:
+    print("INFILTRATION")
+elif result.argmax()==5:
+    print("EFFUSION") 
